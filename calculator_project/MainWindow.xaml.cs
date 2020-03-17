3e28f94a95ebd0ad739c64fce13d4c7d6af56a96
Dovah Viking <DovahViking@hotmail.com>
2020-03-17 09:39:29 +0100

    need '=' case

--- calculator_project/MainWindow.xaml.cs
+++ calculator_project/MainWindow.xaml.cs
@@ -20,15 +20,78 @@ namespace calculator_project
     /// </summary>
     public partial class MainWindow : Window
     {
+        long first;
+        long second;
+        long result;
+        bool finished = false;
+        Operations operation = Operations.none;
+
         public MainWindow()
         {
             WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
             InitializeComponent();
         }
 
-        public void clear()
+        private enum Operations
         {
-            history.Text = string.Empty;
+            none,
+            divide,
+            multiply,
+            minus,
+            plus
+        }
+
+        public void btn_number_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (finished)
+            {
+                history.Text = button.Content.ToString();
+                finished = false;
+            }
+            else if (history.Text == "0")
+            {
+                history.Text = button.Content.ToString();
+            }
+            else
+            {
+                //history.Text = history.Text;
+            }
+        }
+
+        public void btn_operator_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            switch (button.Content.ToString())
+            {
+                case "CE":
+                    history.Text = string.Empty;
+                    break;
+                case "x":
+                    first = Convert.ToInt64(history.Text);
+                    operation = Operations.multiply;
+                    history.Text = string.Empty;
+                    break;
+                case "÷":
+                    first = Convert.ToInt64(history.Text);
+                    operation = Operations.divide;
+                    history.Text = string.Empty;
+                    break;
+                case "+":
+                    first = Convert.ToInt64(history.Text);
+                    operation = Operations.plus;
+                    history.Text = string.Empty;
+                    break;
+                case "-":
+                    first = Convert.ToInt64(history.Text);
+                    operation = Operations.minus;
+                    history.Text = string.Empty;
+                    break;
+                case "=":
+
+            }
         }
     }
 }
